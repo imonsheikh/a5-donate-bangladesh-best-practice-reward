2b779dc688859a884b855b4f85758e2779758568
imon-sheikh <mdimonhossain2017@gmail.com>
2024-10-17 19:31:07 +0600

    added history button funtionallity and modal

--- js/main.js
+++ js/main.js
@@ -18,6 +18,26 @@ donateBtns.forEach((btn) => {
       
 })
 
+
+const addToDonateHistory = (amountBalance, cardTitle) =>{
+   const historyContainer = document.getElementById('history-container')
+
+   const historyEntry = document.createElement('div')
+   historyEntry.classList.add('border', 'p-4', 'rounded-lg', 'shadow-sm')
+   historyEntry.innerHTML = `
+   <h3 class ="font-semibold">Title: ${cardTitle}</h3>
+   <p>Donation Amount: ${amountBalance}</p>
+   <p>Date: ${new Date().toLocaleString()}</p>
+   <p>Date: ${new Date().toDateString()}</p>
+   <p>Date: ${new Date().toLocaleDateString()}</p>
+   <p>Date: ${new Date().toLocaleTimeString()}</p>
+   `
+
+   historyContainer.appendChild(historyEntry)
+}
+
+
+// Function 2 
 const handleDonate = (button) =>{
 // console.log(button); 
 const cardElement = button.closest(".card")
@@ -31,7 +51,6 @@ if(isNaN(amountBalance) || amountBalance <= 0 || amountBalance > navbarBalance){
 alert('Please give valid input')
 return
 }
-
 updateNavbarBalance(amountBalance);
 
 const cardsBalance = parseFloat(cardElement.querySelector('.card-balance').innerText)
@@ -41,10 +60,16 @@ const newCardBalance = cardsBalance + amountBalance
 console.log(newCardBalance);
 cardElement.querySelector('.card-balance').innerText = newCardBalance.toFixed(2)
 
+const cardTitle = cardElement.querySelector('.card-title').innerText
+// console.log(cardTitle);
+addToDonateHistory(amountBalance, cardTitle);
 
 cardElement.querySelector('input').value= ""
+document.getElementById('show-modal-btn').showModal()
 
 }
+
+
 
 const updateNavbarBalance = (amountBalance) =>{
 // console.log(amountBalance);
